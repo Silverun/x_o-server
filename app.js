eae04aaa0b7f955f9cf14bd047b60701695f11f8
silverun <imonutiy@gmail.com>
2023-03-08 21:36:57 +0300

    Board start

--- app.js
+++ app.js
@@ -1,54 +1,87 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
 const cors = require("cors");
-const sessionManager = require("./middleware/sessions");
+const { sessionManager, sessionStore } = require("./middleware/sessions");
 
 const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
-    // credentials: true,
+    origin: ["http://localhost:3000", "https://admin.socket.io"],
+    credentials: true,
   },
 });
 
-// io.use((socket, next) => {
-//   const username = socket.handshake.auth.username;
-//   if (!username) {
-//     return next(new Error("invalid username"));
-//   }
-//   socket.username = username;
-//   next();
-// });
+instrument(io, {
+  auth: false,
+  mode: "development",
+  store: sessionStore,
+});
 
 io.use(sessionManager);
 
 io.on("connection", async (socket) => {
-  socket.join(socket.userID);
-
-  socket.broadcast.emit("user_connected", {
-    userID: socket.id,
+  // persist session
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
     username: socket.username,
+    connected: true,
   });
 
+  // emit session details
   socket.emit("session", {
     sessionID: socket.sessionID,
     userID: socket.userID,
   });
+  // join the "userID" room on connection
+  socket.join(socket.userID);
 
   const users = [];
-  for (let [id, socket] of io.of("/").sockets) {
+  sessionStore.findAllSessions().forEach((session) => {
     users.push({
-      userID: id,
-      username: socket.username,
+      userID: session.userID,
+      username: session.username,
+      connected: session.connected,
     });
-  }
+  });
   socket.emit("users", users);
+  // notify existing users
+  socket.broadcast.emit("user_connected", {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true,
+  });
+
+  socket.on("disconnect", async () => {
+    console.log("disconnect ran");
+    const matchingSockets = await io.in(socket.userID).fetchSockets();
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // notify other users
+      console.log("is disc ran");
+      socket.broadcast.emit("user_disconnected", socket.userID);
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false,
+      });
+    }
+  });
 
   socket.on("invite_player", (id) => {
-    socket.to(id).emit("game_invitation", { from: socket.id });
+    socket.to(id).emit("game_invitation", {
+      from: socket.userID,
+      by: socket.username,
+    });
+  });
+
+  socket.on("join_room", (fromId) => {
+    socket.join(fromId);
+    socket.to(fromId).emit("game_accepted");
   });
 });
 
--- middleware/sessions.js
+++ middleware/sessions.js
@@ -4,10 +4,8 @@ const { InMemorySessionStore } = require("../store/sessionStore");
 const sessionStore = new InMemorySessionStore();
 
 const sessionManager = (socket, next) => {
-  console.log("sessions ran");
-  console.log("socket handshake auth", socket.handshake.auth);
-
   const sessionID = socket.handshake.auth.sessionID;
+  console.log("SessionID", sessionID);
   if (sessionID) {
     // find existing session
     const session = sessionStore.findSession(sessionID);
@@ -21,15 +19,20 @@ const sessionManager = (socket, next) => {
     }
   }
   const username = socket.handshake.auth.username;
+
   if (!username) {
-    console.log("invalid username");
+    console.log("Invalid username");
     return next(new Error("invalid username"));
   }
   // create new session
   socket.sessionID = uuidv4();
   socket.userID = uuidv4();
   socket.username = username;
+
   next();
 };
 
-module.exports = sessionManager;
+module.exports = {
+  sessionStore,
+  sessionManager,
+};
